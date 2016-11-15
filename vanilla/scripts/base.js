875323904d2d53e62487f90ed811676f32028cd3
Lahcen Zbair <lahcen.zbair@nespresso.com>
2016-11-15 07:13:14 +0000

    a little bit of native

---
+++ vanilla/scripts/base.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var products = [
+{
+  title: "PRODUCT-1",
+  price: "105"
+},
+{
+  title: "PRODUCT-2",
+  price: "77"
+}
+,
+{
+  title: "PRODUCT-3",
+  price: "214"
+}
+];
+
+(function(window, document){
+get('products').innerHTML = load(products);
+get('search').onkeyup = function(event) {get('products').innerHTML = search(String.fromCharCode(event.keyCode))};
+
+function search(value) {
+  return  load(products.filter(function(p) {
+    return p['title'].includes(value); }));
+}
+
+function load(source){
+  return source.map(function(p) { return p['title'] })
+               .join("<br/>");
+}
+function get (id) { return document.getElementById(id); }
+
+})(window, document);
