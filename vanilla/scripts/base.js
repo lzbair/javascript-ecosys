88d4804f45c06410a82a2b69b3d6628684f2de35
Zbair,Lahcen,NNSA LAUSANNE,GLOBE-DSU-Sol Delivery-Dev Mgmt <Lahcen.Zbair@nespresso.com>
2016-11-17 13:03:39 +0000

    Add additional filters

--- vanilla/scripts/base.js
+++ vanilla/scripts/base.js
@@ -1,34 +1,73 @@
 "use strict";
 
 var products = [
-{
-  title: "PRODUCT-1",
-  price: "105"
-},
-{
-  title: "PRODUCT-2",
-  price: "77"
-}
-,
-{
-  title: "PRODUCT-3",
-  price: "214"
-}
+        {
+            "title": "Savon",
+            "price": 100
+        },
+        {
+            "title": "Parfums",
+            "price": 50
+        },
+        {
+            "title": "Réfrigérateur",
+            "price": 2000
+        },
+        {
+            "title": "Télévision",
+            "price": 3000
+        },
+        {
+            "title": "Ordinateur",
+            "price": 1500
+        },
+        {
+            "title": "Casque",
+            "price": 150
+        }
 ];
 
 (function(window, document){
-get('products').innerHTML = load(products);
-get('search').oninput = function(event) {get('products').innerHTML = search(event.target['value'])};
+var catalog = function(){ return products.slice()};
+get('products').innerHTML = load(catalog());
+    
+get('title').oninput = function(event) {get('products').innerHTML = search(
+    function(p) {return p['title'].includes(event.target['value']); })};
+    
+get('price').oninput = function(event) {get('products').innerHTML = search(
+    function(p) {return p['price'] >= event.target['value']; })};
+    
+get('sort').onchange = function(event) {get('products').innerHTML = sort(event.target['value']);
+                                        get('clear').style.display = "";};
+    
+get('clear').onclick = function(event) {location.reload(true);}
 
-function search(value) {
-  return  load(products.filter(function(p) {
-    return p['title'].includes(value); }));
+
+function search(predicate) {
+  return  load(catalog().filter(predicate));
+}
+    
+    
+function sort(by){
+    if(by == "a") return asc();
+    else if(by == "d") return dsc();
+    else return load(catalog());
+}
+function asc() {
+  return  load(catalog().sort(PRICE_ASC));
+}
+    
+function dsc() {
+  return  load(catalog().sort(PRICE_ASC).reverse());
 }
 
 function load(source){
-  return source.map(function(p) { return p['title'] })
+  return source.map(function(p) { return p['title'] +"<i> (price:" + p['price'] + ")</i>"})
                .join("<br/>");
 }
+    
+var PRICE_ASC = function(p1, p2) {return p1['price'] <= p2['price'] ? -1 : 1;};
 function get (id) { return document.getElementById(id); }
+
 
 })(window, document);
